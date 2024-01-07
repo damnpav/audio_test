adc957c43cd0f33458517bcf4b9fd583f129b2fc
damnpav <hcanilvap@gmail.com>
2024-01-07 13:06:07 +0300

    flaskapp correction

--- release/flask_app.py
+++ release/flask_app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 from db_fun import initialize_cursor, add_order
+from datetime import datetime as dt
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        initialize_cursor()
-        add_order()  # Call the function to insert a row
+        cursor, conn = initialize_cursor()  # Initialize the cursor
+        add_order(dt.now().strftime('%H%M%S%s%d%m'), cursor, conn)
         return redirect(url_for('index'))
-    return render_template('index.html')
+    return render_template('button.html')
 
 
 if __name__ == "__main__":
-    database.init_db()  # Initialize the database
     app.run(debug=True)
